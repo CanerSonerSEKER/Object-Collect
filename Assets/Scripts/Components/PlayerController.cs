809bc91757dbdc2218a4518ef0dadbd73f38e00a
canersonerseker <canerseker444@gmail.com>
2024-10-26 23:53:45 +0300

    PlayerContoller added + multiplayer system was created using photon

---
+++ Assets/Scripts/Components/PlayerController.cs
@@ -0,0 +1,89 @@
+using System;
+using Photon.Pun;
+using UnityEngine;
+
+namespace Components
+{
+    public class PlayerController : MonoBehaviour
+    {
+        [SerializeField] private GameObject _cameraHolder; 
+        [SerializeField] private float _mouseSensitivity, _sprintSpeed, _walkSpeed, _jumpForce, _smoothTime;
+
+        private float _verticalLookRotation;
+        private bool _grounded;
+        private Vector3 _smoothMoveVelocity;
+        private Vector3 _moveAmount;
+        
+        private Rigidbody _rigidbody;
+        private PhotonView _photonView;
+
+        private void Awake()
+        {
+            _rigidbody = GetComponent<Rigidbody>();
+            _photonView = GetComponent<PhotonView>();
+        }
+
+        private void Start()
+        {
+            if (_photonView.IsMine) return;
+            Destroy(GetComponentInChildren<Camera>().gameObject);
+            Destroy(_rigidbody);
+        }
+
+        private void Update()
+        {
+            if (!_photonView.IsMine)
+            {
+                return;
+            }
+            
+            Look();
+
+            Move();
+
+            Jump();
+            
+        }
+
+        private void Look()
+        {
+            transform.Rotate(Vector3.up * Input.GetAxisRaw("Mouse X") * _mouseSensitivity);
+
+            _verticalLookRotation += Input.GetAxisRaw("Mouse Y") * _mouseSensitivity;
+            _verticalLookRotation = Mathf.Clamp(_verticalLookRotation, -90f, 90f);
+
+            _cameraHolder.transform.localEulerAngles = Vector3.left * _verticalLookRotation;
+        }
+        
+        private void Move()
+        {
+            Vector3 moveDir = new Vector3(Input.GetAxisRaw("Horizontal"), 0 , Input.GetAxisRaw("Vertical")).normalized;
+
+            _moveAmount = Vector3.SmoothDamp(_moveAmount, moveDir * (Input.GetKey(KeyCode.LeftShift) ? _sprintSpeed : _walkSpeed), ref _smoothMoveVelocity, _smoothTime);
+        }
+
+        private void Jump()
+        {
+            if (Input.GetKeyDown(KeyCode.Space) && _grounded)
+            {
+                _rigidbody.AddForce(transform.up * _jumpForce);
+            }
+        }
+
+        public void SetGroundedState(bool grounded)
+        {
+            _grounded = grounded;
+            
+        }
+
+        private void FixedUpdate()
+        { 
+            if (!_photonView.IsMine)
+            {
+                return;
+            }
+            
+            _rigidbody.MovePosition(_rigidbody.position + transform.TransformDirection(_moveAmount) * Time.fixedDeltaTime);
+        }
+    }
+}
--- Assets/Scripts/Components/PlayerManager.cs
+++ Assets/Scripts/Components/PlayerManager.cs
@@ -1,19 +1,30 @@
+using System;
+using System.IO;
+using Photon.Pun;
 using UnityEngine;
 
 namespace Components
 {
     public class PlayerManager : MonoBehaviour
     {
-        // Start is called before the first frame update
-        void Start()
+        private PhotonView _pv;
+
+        private void Awake()
         {
-        
+            _pv = GetComponent<PhotonView>();
         }
 
-        // Update is called once per frame
-        void Update()
+        private void Start()
         {
-        
+            if (_pv.IsMine)
+            {
+                CreateController();
+            }
+        }
+
+        private void CreateController()
+        {
+            PhotonNetwork.Instantiate(Path.Combine("PhotonPrefabs", "PlayerController"), Vector3.zero, Quaternion.identity);
         }
     }
 }
